11e620544728f1026dc44eb22ab6f2714d6299fb
xzp <3094997376@qq.com>
2022-05-23 11:29:13 +0800

    初始化

---
+++ Assets/Scripts/SceneMgr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SceneMgr : NetworkBehaviour
+{
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+   
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width/2,Screen.height/2,100,100),"跳转到大厅"))
+        {
+            FindObjectOfType<NetworkManager>().ServerChangeScene(sceneName);
+        }
+    }
+}
